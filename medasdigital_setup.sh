df20701c8a859f2a9b0256580a3de914c4e6de99
MedasDigital <111655819+oxygene76@users.noreply.github.com>
2024-11-09 15:27:44 +0100

    Update medasdigital_setup.sh

--- medasdigital_setup.sh
+++ medasdigital_setup.sh
@@ -11,63 +11,61 @@ PERSISTENT_PEERS="peer1@ip1:26656,peer2@ip2:26656"  # Add your persistent peers 
 GENESIS_URL="https://raw.githubusercontent.com/oxygene76/medasdigital-2/refs/heads/main/genesis/mainnet/config/genesis.json"
 
 # ASCII Banner
-
-echo"'##::::'##:'########:'########:::::'###:::::'######::                                          "
-echo" ###::'###: ##.....:: ##.... ##:::'## ##:::'##... ##:                                          "
-echo"####'####: ##::::::: ##:::: ##::'##:. ##:: ##:::..::                                           "
-echo" ## ### ##: ######::: ##:::: ##:'##:::. ##:. ######::                                          "
-echo" ##. #: ##: ##...:::: ##:::: ##: #########::..... ##:                                          "
-echo" ##:.:: ##: ##::::::: ##:::: ##: ##.... ##:'##::: ##:                                          "
-echo" ##:::: ##: ########: ########:: ##:::: ##:. ######::                                          "
-echo"..:::::..::........::........:::..:::::..:::......:::                                          "
-echo"'########::'####::'######:::'####:'########::::'###::::'##:::::::::::'#######:::::::::'#####:::"
-echo"##.... ##:. ##::'##... ##::. ##::... ##..::::'## ##::: ##::::::::::'##.... ##:::::::'##.. ##::"
-echo" ##:::: ##:: ##:: ##:::..:::: ##::::: ##:::::'##:. ##:: ##::::::::::..::::: ##::::::'##:::: ##:"
-echo" ##:::: ##:: ##:: ##::'####:: ##::::: ##::::'##:::. ##: ##:::::::::::'#######::::::: ##:::: ##:"
-echo" ##:::: ##:: ##:: ##::: ##::: ##::::: ##:::: #########: ##::::::::::'##::::::::::::: ##:::: ##:"
-echo" ##:::: ##:: ##:: ##::: ##::: ##::::: ##:::: ##.... ##: ##:::::::::: ##::::::::'###:. ##:: ##::"
-echo" ########::'####:. ######:::'####:::: ##:::: ##:::: ##: ########:::: #########: ###::. #####:::"
-echo"........:::....:::......::::....:::::..:::::..:::::..::........:::::.........::...::::.....::::"
-
+echo "'##::::'##:'########:'########:::::'###:::::'######::                                          "
+echo " ###::'###: ##.....:: ##.... ##:::'## ##:::'##... ##:                                          "
+echo " ####'####: ##::::::: ##:::: ##::'##:. ##:: ##:::..::                                          "
+echo " ## ### ##: ######::: ##:::: ##:'##:::. ##:. ######::                                          "
+echo " ##. #: ##: ##...:::: ##:::: ##: #########::..... ##:                                          "
+echo " ##:.:: ##: ##::::::: ##:::: ##: ##.... ##:'##::: ##:                                          "
+echo " ##:::: ##: ########: ########:: ##:::: ##:. ######::                                          "
+echo "..:::::..::........::........:::..:::::..:::......:::                                          "
+echo "'########::'####::'######:::'####:'########::::'###::::'##:::::::::::'#######:::::::::'#####:::"
+echo " ##.... ##:. ##::'##... ##::. ##::... ##..::::'## ##::: ##::::::::::'##.... ##:::::::'##.. ##::"
+echo " ##:::: ##:: ##:: ##:::..:::: ##::::: ##:::::'##:. ##:: ##::::::::::..::::: ##::::::'##:::: ##:"
+echo " ##:::: ##:: ##:: ##::'####:: ##::::: ##::::'##:::. ##: ##:::::::::::'#######::::::: ##:::: ##:"
+echo " ##:::: ##:: ##:: ##::: ##::: ##::::: ##:::: #########: ##::::::::::'##::::::::::::: ##:::: ##:"
+echo " ##:::: ##:: ##:: ##::: ##::: ##::::: ##:::: ##.... ##: ##:::::::::: ##::::::::'###:. ##:: ##::"
+echo " ########::'####:. ######:::'####:::: ##:::: ##:::: ##: ########:::: #########: ###::. #####:::"
+echo "........:::....:::......::::....:::::..:::::..:::::..::........:::::.........::...::::.....::::"
+echo
 echo "Welcome to Medas Digital 2.0!"
+
+# Funktion, um nach jeder Aktion auf Eingabe zu warten
+pause() {
+    read -p "Press Return to continue..."
+}
 
 # Function to download and install the latest binary from GitHub and set up CosmWasm library
 setup_node() {
     clear
     echo "Fetching latest MedasDigital binary..."
-    # Fetch the latest release version
     LATEST_RELEASE=$(curl -s "https://api.github.com/repos/oxygene76/medasdigital-2/releases/latest" | grep "tag_name" | cut -d '"' -f 4)
     echo "Latest version: $LATEST_RELEASE"
 
-    # Download the binary
     wget "$REPO/download/$LATEST_RELEASE/medasdigitald" -O medasdigitald
     chmod +x medasdigitald
     sudo mv medasdigitald $BIN_DIR/
 
-    # Set up CosmWasm library
     echo "Setting up CosmWasm library..."
     wget -P /usr/lib https://github.com/CosmWasm/wasmvm/raw/main/internal/api/libwasmvm.x86_64.so
     sudo ldconfig
     echo "CosmWasm library setup complete."
 
-    # Initialize the node with the specified home directory
     echo "Initializing node in $NODE_HOME..."
     medasdigitald init "MedasDigitalNode" --chain-id $CHAIN_ID --home $NODE_HOME
 
-    # Download and replace the genesis.json file
     echo "Downloading genesis.json from $GENESIS_URL..."
     wget -O $NODE_HOME/config/genesis.json $GENESIS_URL
     echo "Genesis file downloaded and set up at $NODE_HOME/config/genesis.json."
 
-    # Set minimum gas price and denom in app.toml
     echo "Setting minimum gas price and denom in app.toml..."
     sed -i "s/^minimum-gas-prices =.*/minimum-gas-prices = \"$MIN_GAS_PRICE$GAS_DENOM\"/" $NODE_HOME/config/app.toml
 
-    # Set persistent peers and other configurations in config.toml
     echo "Setting persistent peers and other configurations in config.toml..."
     sed -i "s/^persistent_peers =.*/persistent_peers = \"$PERSISTENT_PEERS\"/" $NODE_HOME/config/config.toml
 
     echo "Node setup complete. Configuration is located in $NODE_HOME"
+    pause
 }
 
 # Function to set up a validator
@@ -80,13 +78,11 @@ setup_validator() {
     read -p "Enter your commission max change rate (e.g., 0.01): " COMMISSION_MAX_CHANGE
     read -p "Enter your validator moniker (name): " MONIKER_NAME
 
-    # Check if wallet exists
     if ! medasdigitald keys show $WALLET_NAME --home $NODE_HOME > /dev/null 2>&1; then
         echo "Wallet $WALLET_NAME does not exist. Please create it first."
         exit 1
     fi
 
-    # Create validator with custom moniker
     echo "Creating validator with moniker: $MONIKER_NAME"
     medasdigitald tx staking create-validator \
       --amount $STAKE_AMOUNT \
@@ -99,6 +95,7 @@ setup_validator() {
       --moniker "$MONIKER_NAME" \
       --chain-id $CHAIN_ID \
       --home $NODE_HOME
+    pause
 }
 
 # Function to create a wallet
@@ -106,6 +103,7 @@ create_wallet() {
     clear
     read -p "Enter a wallet name: " WALLET_NAME
     medasdigitald keys add $WALLET_NAME --home $NODE_HOME
+    pause
 }
 
 # Function to import a wallet from mnemonic or private key
@@ -130,12 +128,14 @@ import_wallet() {
     else
         echo "Invalid option. Please select either 1 or 2."
     fi
+    pause
 }
 
 # Function to list wallets
 list_wallets() {
     clear
     medasdigitald keys list --home $NODE_HOME
+    pause
 }
 
 # Display menu
